a93836b1fe02f5f58b33e86ca842ce8c41a3780b
Duarte Cancela <duartecancela@gmail.com>
2022-10-19 11:08:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.intents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var number: Int = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewCount = findViewById<TextView>(R.id.textViewCount)
+        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+        val editTextPersonName = findViewById<EditText>(R.id.editTextTextPersonName)
+        val buttonNext = findViewById<Button>(R.id.buttonNext)
+
+        buttonPlus.setOnClickListener {
+            number++
+            textViewCount.text = number.toString()
+        }
+
+        buttonMinus.setOnClickListener {
+            number--
+            textViewCount.text = number.toString()
+        }
+
+        buttonNext.setOnClickListener {
+            val n =  editTextPersonName.text.toString()
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("name", n)
+            intent.putExtra("number", number)
+            startActivity(intent)
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/intents/ResultActivity.kt
@@ -0,0 +1,23 @@
+package com.example.intents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val number = intent.getIntExtra("number", 0)
+        val name = intent.getStringExtra("name")
+
+        val textViewCountResult = findViewById<TextView>(R.id.textViewCountResult)
+        val textViewPersonNameResult = findViewById<TextView>(R.id.textViewPersonNameResult)
+
+        textViewCountResult.text = number.toString()
+        textViewPersonNameResult.text = name
+
+    }
+}
